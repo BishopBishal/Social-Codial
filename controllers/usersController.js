8d96083ac086879705da8f2d869413a3e70d1eae
bishop <bishopbishal256@gmail.com>
2023-07-06 18:13:45 +0530

    add manual authentication

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,30 @@
 const User =require("../models/User");
-module.exports.profile=function(req, res){
-    return res.render('userProfile',{title : 'ProfilePage'});
+module.exports.profile=async function(req, res){
+    try
+    {
+        if(Object.keys(req.cookies).length > 0)
+        {
+                console.log('requested cookies:- ',req.cookies);
+                const user =await User.findById(req.cookies.user_id);
+                console.log(user);
+                if(user != null){
+                    return res.render('userProfile',{
+                        title: "Profile",
+                        userList : [user]
+                });
+                }
+            
+            
+        }
+        else{
+            console.log("No cookie for user ");
+            return res.redirect('/user/signin');
+        }
+    }
+    catch(err){
+        return res.status(500).send('Internal Server Error');
+    }
+
 }
 
 
@@ -39,12 +63,35 @@ module.exports.createUser=async function(req, res){
     }
     catch(err)
     {
-        return  res.status(500).send("Internal Server Error",err);
+        return  res.status(500).send("Internal Server Error"+err);
     }
 };
 
-module.exports.createSession=function(req, res){
+module.exports.createSession=async function(req, res){
     //Do it later
-    console.log('createSession',req.body);
-    return res.redirect('back');
+    // console.log('createSession',req.body);
+    // return res.redirect('back');
+    try{
+        //find the user
+        const olduser=await User.findOne({email : req.body.email, password : req.body.password});
+        console.log('user found',olduser,req.body);
+          
+        //handle user found
+        if(olduser != null)
+        {
+           
+              //handle session creation which means cookies
+              res.cookie('user_id',olduser.id);
+              return res.redirect('/user/profile');
+
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+       
+    }
+    catch(err){
+        return res.status(500).send("Internal Server Error"+err);
+    }
 };
